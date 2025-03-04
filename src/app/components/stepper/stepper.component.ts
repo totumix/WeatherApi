c40f3d655ba5186811aa71eea16a7c167c36143b
totumix <totucr95@gmail.com>
2025-03-04 00:14:15 -0500

    mejora en el routerLink del stepper

--- src/app/components/stepper/stepper.component.ts
+++ src/app/components/stepper/stepper.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-stepper',
@@ -11,15 +11,29 @@ import { Router, RouterModule } from '@angular/router';
 export class StepperComponent {
 
   steps = [
-    { route: '/detail', label: 'Ver Detalles' },
+    { route: '/detail', label: 'Detalles' },
     { route: '/favorites', label: 'Favoritos' },
-    { route: '/record', label: 'Historias' }];
+    { route: '/record', label: 'Historial' }
+  ];
   selectedStep = 0;
-  constructor(private router: Router) {
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.setActiveStep();
+    this.router.events.subscribe(() => {
+      this.setActiveStep();
+    });
   }
+
+  setActiveStep() {
+    const currentRoute = this.router.url;
+    const matchedStep = this.steps.findIndex(step => currentRoute.includes(step.route));
+    this.selectedStep = matchedStep !== -1 ? matchedStep : 0; 
+  }
+
   selectStep(index: number, route: string) {
-    this.router.navigate([route])
+    this.router.navigate([route]);
     this.selectedStep = index;
   }
 }
