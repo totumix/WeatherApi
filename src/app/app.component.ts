6f39c4661e14f568fe39e479084906513790db57
totumix <totucr95@gmail.com>
2025-03-04 00:09:41 -0500

    app module config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AutoCompleteComponent } from './components/auto-complete/auto-complete.component';
+import { StepperComponent } from "./components/stepper/stepper.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, AutoCompleteComponent, StepperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'detail', pathMatch: 'full' },
+  { path: 'detail', loadComponent: () => import('./pages/detail/detail.component').then(m => m.DetailComponent) },
+  { path: 'favorites', loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent) },
+  { path: 'record', loadComponent: () => import('./pages/record/record.component').then(m => m.RecordComponent) }
+];
